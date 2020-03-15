be813c7be5fcf8cb5a1aad6ba8c89ef7822a7a6f
Fabian <fschaffenrath@gmail.com>
2020-03-16 00:54:19 +0100

    Implemented 2.1 (inkl. UI Elements for 2.2)

--- app/src/main/java/com/se2/einzelbeispielschaffenrath/MainActivity.java
+++ app/src/main/java/com/se2/einzelbeispielschaffenrath/MainActivity.java
@@ -1,13 +1,41 @@
 package com.se2.einzelbeispielschaffenrath;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+    Button submitBtn;
+    EditText inputMatNr;
+    TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitBtn = findViewById(R.id.submitBtn);
+        inputMatNr = findViewById(R.id.inputMatNr);
+        answerText = findViewById(R.id.answerText);
+
+        handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(Message msg){
+                answerText.setText((String)(msg.obj));
+            }
+        };
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MatNrThread thread = new MatNrThread(inputMatNr.getText().toString(),handler);
+                thread.start();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/se2/einzelbeispielschaffenrath/MatNrThread.java
@@ -0,0 +1,42 @@
+package com.se2.einzelbeispielschaffenrath;
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class MatNrThread extends Thread {
+
+    Socket clientSocket;
+    DataOutputStream outToServer;
+    BufferedReader inFromServer;
+    String input;
+    Handler handler;
+
+    public MatNrThread(String input, Handler handler){
+        this.input = input;
+        this.handler = handler;
+    }
+
+    @Override
+    public void run(){
+        try {
+            clientSocket = new Socket("se2-isys.aau.at",53212);
+            outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outToServer.writeBytes(input + "\n");
+            Message msg = Message.obtain();
+            msg.obj = inFromServer.readLine();
+            handler.sendMessage(msg);
+            clientSocket.close();
+        } catch (IOException ex) {
+            Log.e("THREADERROR", "Fehler im Übertragungsthread.");
+        }
+    }
+
+}
